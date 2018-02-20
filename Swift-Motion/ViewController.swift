949f7c4189c22fb3cd7a5a0c547e21f24c8c4cf6
ginshari <16682509+ginshari@users.noreply.github.com>
2018-02-20 13:38:45 +0900

    XCode9で動かなかったのでエラーをとりあえず解消するまで

--- Swift-Motion/ViewController.swift
+++ Swift-Motion/ViewController.swift
@@ -31,29 +31,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Initialize MotionManager
         motionManager.deviceMotionUpdateInterval = 0.05 // 20Hz
         
         // Start motion data acquisition
-        motionManager.startDeviceMotionUpdatesToQueue( NSOperationQueue.currentQueue(), withHandler:{
+        // motionManager.startDeviceMotionUpdatesToQueue( OperationQueue.current!, withHandler:{
+        motionManager.startDeviceMotionUpdates( to:OperationQueue.current!, withHandler:{
             deviceManager, error in
-            var accel: CMAcceleration = deviceManager.userAcceleration
+            let accel: CMAcceleration = deviceManager!.userAcceleration
             self.acc_x.text = String(format: "%.2f", accel.x)
             self.acc_y.text = String(format: "%.2f", accel.y)
             self.acc_z.text = String(format: "%.2f", accel.z)
 
-            var gyro: CMRotationRate = deviceManager.rotationRate
+            let gyro: CMRotationRate = deviceManager!.rotationRate
             self.gyro_x.text = String(format: "%.2f", gyro.x)
             self.gyro_y.text = String(format: "%.2f", gyro.y)
             self.gyro_z.text = String(format: "%.2f", gyro.z)
 
-            var attitude: CMAttitude = deviceManager.attitude
+            let attitude: CMAttitude = deviceManager!.attitude
             self.attitude_roll.text = String(format: "%.2f", attitude.roll)
             self.attitude_pitch.text = String(format: "%.2f", attitude.pitch)
             self.attitude_yaw.text = String(format: "%.2f", attitude.yaw)
 
-            var quaternion: CMQuaternion = attitude.quaternion
+            let quaternion: CMQuaternion = attitude.quaternion
             self.attitude_x.text = String(format: "%.2f", quaternion.x)
             self.attitude_y.text = String(format: "%.2f", quaternion.y)
             self.attitude_z.text = String(format: "%.2f", quaternion.z)
