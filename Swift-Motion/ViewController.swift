a3ef552779b1658a4111a5237adab03dba149515
takuyamakawa <g2117052@fun.ac.jp>
2018-02-21 17:13:33 +0900

    test label 2.5

--- Swift-Motion/ViewController.swift
+++ Swift-Motion/ViewController.swift
@@ -21,6 +21,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var y_25: UILabel!
     @IBOutlet weak var z_25: UILabel!
     
+    var lab_x25 = 0
+    var lab_y25 = 0
+    var lab_z25 = 0
     
     // create instance of MotionManager
     let motionManager: CMMotionManager = CMMotionManager()
@@ -40,9 +43,20 @@ class ViewController: UIViewController {
             self.gyro_x.text = String(format: "%.2f", gyro.x)
             self.gyro_y.text = String(format: "%.2f", gyro.y)
             self.gyro_z.text = String(format: "%.2f", gyro.z)
+            
+            if gyro.x > 2.5{
+                self.lab_x25 += 1
+                self.x_25.text = String(format: "%d", self.lab_x25)
+            }
+            if gyro.y > 2.5{
+                self.lab_y25 += 1
+                self.y_25.text = String(format: "%d", self.lab_y25)
+            }
+            if gyro.y > 2.5{
+                self.lab_y25 += 1
+                self.y_25.text = String(format: "%d", self.lab_y25)
+            }
         })
-        
-        
         
     }
 
