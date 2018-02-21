a07d04d78e9612eca445b8800989ecb11e560917
takuyamakawa <g2117052@fun.ac.jp>
2018-02-21 16:48:12 +0900

    add label 2.5

--- Swift-Motion/ViewController.swift
+++ Swift-Motion/ViewController.swift
@@ -12,20 +12,16 @@ import CoreMotion
 class ViewController: UIViewController {
     
     // Connection with interface builder
-    @IBOutlet var acc_x: UILabel!
-    @IBOutlet var acc_y: UILabel!
-    @IBOutlet var acc_z: UILabel!
+
     @IBOutlet var gyro_x: UILabel!
     @IBOutlet var gyro_y: UILabel!
     @IBOutlet var gyro_z: UILabel!
-    @IBOutlet var attitude_roll: UILabel!
-    @IBOutlet var attitude_pitch: UILabel!
-    @IBOutlet var attitude_yaw: UILabel!
-    @IBOutlet var attitude_x: UILabel!
-    @IBOutlet var attitude_y: UILabel!
-    @IBOutlet var attitude_z: UILabel!
-    @IBOutlet var attitude_w: UILabel!
 
+    @IBOutlet weak var x_25: UILabel!
+    @IBOutlet weak var y_25: UILabel!
+    @IBOutlet weak var z_25: UILabel!
+    
+    
     // create instance of MotionManager
     let motionManager: CMMotionManager = CMMotionManager()
 
@@ -39,28 +35,15 @@ class ViewController: UIViewController {
         // motionManager.startDeviceMotionUpdatesToQueue( OperationQueue.current!, withHandler:{
         motionManager.startDeviceMotionUpdates( to:OperationQueue.current!, withHandler:{
             deviceManager, error in
-            let accel: CMAcceleration = deviceManager!.userAcceleration
-            self.acc_x.text = String(format: "%.2f", accel.x)
-            self.acc_y.text = String(format: "%.2f", accel.y)
-            self.acc_z.text = String(format: "%.2f", accel.z)
-
+            
             let gyro: CMRotationRate = deviceManager!.rotationRate
             self.gyro_x.text = String(format: "%.2f", gyro.x)
             self.gyro_y.text = String(format: "%.2f", gyro.y)
             self.gyro_z.text = String(format: "%.2f", gyro.z)
-
-            let attitude: CMAttitude = deviceManager!.attitude
-            self.attitude_roll.text = String(format: "%.2f", attitude.roll)
-            self.attitude_pitch.text = String(format: "%.2f", attitude.pitch)
-            self.attitude_yaw.text = String(format: "%.2f", attitude.yaw)
-
-            let quaternion: CMQuaternion = attitude.quaternion
-            self.attitude_x.text = String(format: "%.2f", quaternion.x)
-            self.attitude_y.text = String(format: "%.2f", quaternion.y)
-            self.attitude_z.text = String(format: "%.2f", quaternion.z)
-            self.attitude_w.text = String(format: "%.2f", quaternion.w)
         })
-
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
